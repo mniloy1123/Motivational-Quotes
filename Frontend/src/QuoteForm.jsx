36c7dc448c79accbf3994ff852dbbc1dc4e2cab2
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-10-07 22:42:43 -0400

    Update form styling

--- Frontend/src/QuoteForm.jsx
+++ Frontend/src/QuoteForm.jsx
@@ -11,8 +11,8 @@ export default function QuoteForm() {
         <textarea
           id="about"
           name="about"
-          rows="3"
-          className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          rows="6"
+          className="block w-full rounded-md border-0 px-1.5 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6"
         ></textarea>
       </div>
       <button
