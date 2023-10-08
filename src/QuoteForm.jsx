ccf089687afadec1f14cea67e532a3ae5b8d1c30
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-10-07 22:15:31 -0400

    Add button to submit form

--- src/QuoteForm.jsx
+++ src/QuoteForm.jsx
@@ -15,6 +15,12 @@ export default function QuoteForm() {
           className="block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         ></textarea>
       </div>
+      <button
+        type="submit" // Change to submit if it's meant to submit a form.
+        className="rounded-md bg-white mt-4 px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:ring-2 focus:ring-blue-500 focus:outline-none active:bg-gray-100">
+        Submit
+      </button>
+
       <p className="mt-3 text-m leading-6 text-rose-400">
         Give us something motivational.
       </p>
